5744c796013bba47fd161aa1588b16d04cbada2d
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-10-13 20:51:48 -0300

    Nginx docker initial commit

---
+++ nginx/nginx-configuration-files/setup.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -s /etc/nginx/sites-available/jovanibrasil.com.conf /etc/nginx/sites-enabled/jovanibrasil.com.conf
+ln -s /etc/nginx/sites-available/blog.jovanibrasil.com.conf /etc/nginx/sites-enabled/blog.jovanibrasil.com.conf
+ln -s /etc/nginx/sites-available/notes.jovanibrasil.com.conf /etc/nginx/sites-enabled/notes.jovanibrasil.com.conf
+
