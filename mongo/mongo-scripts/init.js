d62eb0d2fa1e58c0a99056ecedc72ec28b5b940c
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-09-06 18:56:26 -0300

    Add and adjust files and configs

--- mongo/mongo-scripts/init.js
+++ mongo/mongo-scripts/init.js
@@ -1,9 +1,9 @@
 db.createUser(
-    {
-      user: "admin",
-      pwd: "admin",
-      roles: [ "readWrite", "dbAdmin" ]
-    }
+  {
+    user: "admin",
+    pwd: "admin",
+    roles: [ "readWrite", "dbAdmin" ]
+  }
 )
 
 db.createCollection("users")
@@ -12,13 +12,13 @@ db.createCollection("notebooks");
 db.createCollection("colorPallets");
 
 db.users.insert([{ 
-	_id: "1", userId: "1", userName: "user" 
+_id: "1", userId: "1", userName: "user" 
 }, { 
-	_id: "2", userId: "2", userName: "admin" 
+_id: "2", userId: "2", userName: "admin" 
 }]);
 
 db.colorPallets.insert([{
-  _id: "3", userName: "user", colors: []
+_id: "3", userName: "user", colors: []
 }, {
-  _id: "4", userName: "admin", colors: []
+_id: "4", userName: "admin", colors: []
 }]);
